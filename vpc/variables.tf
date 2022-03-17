385ccaed03e12305ecf61af8162e6903fb7431ac
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-17 13:01:23 -0400

    First modification

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -21,3 +21,25 @@ variable "private_subnets" {
 variable "availability_zones" {
   description = "List of availability zones"
 }
+
+variable "cidr" {
+  type    = string
+  default = "145.0.0.0/16"
+}
+
+variable "azs" {
+  type = list(string)
+  default = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+}
+
+variable "subnets-ip" {
+  type = list(string)
+  default = [
+    "145.0.1.0/24",
+    "145.0.2.0/24"
+  ]
+
+}
