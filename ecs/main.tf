32c13c567aed0fd89d8b51d0b56353a84e95e026
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-23 16:47:38 -0400

    Updated

--- ecs/main.tf
+++ ecs/main.tf
@@ -95,10 +95,10 @@ resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attach
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment" {
-  role       = aws_iam_role.ecs_task_role.name
-  policy_arn = aws_iam_policy.dynamodb.arn
-}
+# resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment" {
+#   role       = aws_iam_role.ecs_task_role.name
+#   policy_arn = aws_iam_policy.dynamodb.arn
+# }
 
 # resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment-for-secrets" {
 #   role       = aws_iam_role.ecs_task_execution_role.name
