87146b5b36c7d3bf8cedaaf47212bcb9b03e27a9
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-24 16:15:00 -0400

    addedp

--- main.tf
+++ main.tf
@@ -145,7 +145,7 @@ resource "aws_db_instance" "arcablanca_pt_rds" {
 #-------------------------------------------------------------------
 resource "aws_db_subnet_group" "arcablanca_pt_dbsubnets" {
   name       = "main"
-  subnet_ids = "${module.vpc.public_subnets.*.id[count.index]}"
+  subnet_ids = module.vpc.public_subnets[count.index]
 
   tags = {
     Name = "Arca-Blanca-PT-dbSubnet-Group"
