34adbf74947fb190ad3d420c9d0f17d10ca98648
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-24 12:19:10 -0400

    get

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ module "ecs" {
     { name = "PORT",
     value = var.container_port }
   ]
-  # container_secrets      = module.secrets.secrets_map
+  ## container_secrets      = module.secrets.secrets_map
   ##aws_ecr_repository_url = module.ecr.aws_ecr_repository_url
   # container_secrets_arns = module.secrets.application_secrets_arn
 }
