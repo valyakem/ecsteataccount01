ce39c82a5399fa3a3d8474869db6d195c7a4e7d7
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-25 00:48:26 -0400

    added more files

--- main.tf
+++ main.tf
@@ -53,7 +53,6 @@ module "alb" {
   vpc_id                    = module.vpc.id
   subnets                   = module.vpc.public_subnets
   environment               = var.environment
-  ecs_task_security_groups  = [module.security_groups.ecs_tasks]
   alb_security_groups       = [module.security_groups.alb]
   alb_tls_cert_arn          = var.tsl_certificate_arn
   health_check_path         = var.health_check_path
