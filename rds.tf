37de5223ee7aa9ffcaecf172b1670ebc23251bdc
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-24 23:39:45 -0400

    Updating the rds

--- rds.tf
+++ rds.tf
@@ -40,13 +40,13 @@ resource "aws_security_group" "arcablanca_rds_sg" {
       from_port         = 5432
       to_port           = 5432
       protocol          = "tcp"
-      security_groups   = ["${aws_security_group.alb.id}"]
+      security_groups   = [module.alb.alb_security_groups[0].id]
   }  
   ingress {
       from_port         = 5433
       to_port           = 5433
       protocol          = "tcp"
-      security_groups = ["${aws_security_group.alb.id}"]
+      security_groups = [module.alb.alb_security_groups[0].id]
   }
   egress {
     from_port        = 0
