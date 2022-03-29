17951ecf577e63a60d2eac94316e12e13997ad0f
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-29 01:19:51 -0400

    added some subnets

--- rds.tf
+++ rds.tf
@@ -22,7 +22,7 @@ resource "aws_db_instance" "arcablanca_pt_rds" {
 #-------------------------------------------------------------------
 resource "aws_db_subnet_group" "arcablanca_pt_rds" {
   name       = "aracablanca-rds"
-  subnet_ids = [module.vpc.private_subnets[0], module.vpc.private_subnets[1]]
+  subnet_ids = var.private_subnets
 
   tags = {
     Name = "My DB subnet group"
