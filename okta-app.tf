36ad95a0ae4f1f80da66316e8701d45f66933ad1
Valentine AKEM <valyakem@yahoo.co.uk>
2022-04-05 05:59:27 -0400

    Okta Implementation

--- okta-app.tf
+++ okta-app.tf
@@ -1,13 +1,3 @@
-resource "okta_group" "example" {
-  name        = "nexgbit-testapp"
-  description = "Next Bit Test App Group"
-}
-
-resource "okta_group_membership" "example" {
-  group_id = "${okta_group.example.id}"
-  user_id  = "okta-dev-25336660"
-}
-
 resource "okta_user_schema" "dob_extension" {
   index  = "date_of_birth"
   title  = "Date of Birth"
