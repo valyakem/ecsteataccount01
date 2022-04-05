5b6b0f58d58d462bfea421891afa5287eea308e9
Valentine AKEM <valyakem@yahoo.co.uk>
2022-04-05 06:03:42 -0400

    Okta

--- okta-app.tf
+++ okta-app.tf
@@ -1,6 +1,27 @@
-resource "okta_user_schema" "dob_extension" {
-  index  = "date_of_birth"
-  title  = "Date of Birth"
-  type   = "string"
-  master = "PROFILE_MASTER"
+resource "okta_group_schema_property" "example" {
+  index       = "customPropertyName"
+  title       = "customPropertyName"
+  type        = "string"
+  description = "My custom property name"
+  master      = "OKTA"
+  scope       = "SELF"
 }
+
+
+resource "okta_user_type" "example" {
+  name   = "example"
+  display_name = "example"
+  description = "example"
+}
+
+resource "okta_user_schema" "example" {
+  index       = "customPropertyName"
+  title       = "customPropertyName"
+  type        = "string"
+  description = "My custom property name"
+  master      = "OKTA"
+  scope       = "SELF"
+  user_type   = "${data.okta_user_type.example.id}"
+}
+
+
