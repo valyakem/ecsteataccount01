cbe9215087d082f4e76017a9f9520bbccfe2ed3e
Valentine AKEM <valyakem@yahoo.co.uk>
2022-04-06 07:48:23 -0400

    Adding more files

--- okta-app.tf
+++ okta-app.tf
@@ -6,14 +6,14 @@ data "aws_secretsmanager_secret_version" "current" {
   secret_id = data.aws_secretsmanager_secret.secrets.id
 }
 
-output "sensitive_example_hash" {
-  value = jsondecode(nonsensitive(data.aws_secretsmanager_secret_version.current.secret_string))
+locals {
+  api_token = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)
 }
 
 provider "okta" {
   org_name  = var.org_name
   base_url  = var.base_url
-  api_token = "${data.aws_secretsmanager_secret_version.current.secret_id}"
+  api_token = "${local.api_token}"
 }
 
 resource "okta_group_schema_property" "example" {
