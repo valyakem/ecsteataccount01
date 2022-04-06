e7cd4ca60346f8ae7e1c3e3805e0a0841e7091e8
Valentine AKEM <valyakem@yahoo.co.uk>
2022-04-06 09:22:26 -0400

    Adding more files

--- okta-app.tf
+++ okta-app.tf
@@ -22,7 +22,7 @@ provider "okta" {
   api_token = local.api_token
 }
 
-resource "okta_group_schema_property" "example" {
+resource "okta_group_schema_property" "nb_okta" {
   index       = "customPropertyName"
   title       = "customPropertyName"
   type        = "string"
@@ -31,22 +31,54 @@ resource "okta_group_schema_property" "example" {
   scope       = "SELF"
 }
 
-
-resource "okta_user_type" "example" {
-  name   = "example"
-  display_name = "example"
-  description = "example"
+resource "okta_group" "usergroup" {
+  name        = "nbUsers"
+  description = "Okta users group"
 }
 
-resource "okta_user_schema_property" "example" {
-  index       = "customPropertyName"
-  title       = "customPropertyName"
-  type        = "string"
-  description = "My custom property name"
-  master      = "OKTA"
-  scope       = "SELF"
-  user_type   = "${okta_user_type.example.id}"
+
+resource "okta_group_roles" "grouprole" {
+  group_id    = "${okta_group.usergroup.id}"
+  admin_roles = ["SUPER_ADMIN", "USER_ADMIN"]
 }
+
+resource "okta_user" "example" {
+  first_name         = "John"
+  last_name          = "Smith"
+  login              = "john.smith@nexgbits.com"
+  email              = "john.smith@nexgbits.com"
+  city               = "Toronto"
+  cost_center        = "10"
+  country_code       = "CA"
+  department         = "IT"
+  display_name       = "Dr. John Smith"
+  division           = "Acquisitions"
+  employee_number    = "111111"
+  honorific_prefix   = "Dr."
+  honorific_suffix   = "Jr."
+  locale             = "en_CA"
+  manager            = "Akem"
+  manager_id         = "222222"
+  middle_name        = "Valentine"
+  mobile_phone       = "1112223333"
+  nick_name          = "valy"
+  organization       = "Nexgbits Inc."
+  postal_address     = "1234 Testing St."
+  preferred_language = "en-us"
+  primary_phone      = "4445556666"
+  profile_url        = "https://www.example.com/profile"
+  second_email       = "john.smith.fun@example.com"
+  state              = "NY"
+  street_address     = "5678 Testing Ave."
+  timezone           = "America/New_York"
+  title              = "Director"
+  user_type          = "Employee"
+  zip_code           = "11111"
+}
+
+
+
+
 
 # output "test" {value = "${data.external.json.result.okta_api}"}
 //okta additions
