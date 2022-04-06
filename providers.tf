ce32f705712ccc9e3e6d842c3c16f782ae81d59e
Valentine AKEM <valyakem@yahoo.co.uk>
2022-04-06 06:01:50 -0400

    Okta test

--- providers.tf
+++ providers.tf
@@ -32,7 +32,7 @@ provider "aws" {
 }
 
 provider "okta" {
-  org_name  = var.org_name
-  base_url  = var.base_url
-  api_token = var.api_token
+  okta_org_name  = var.org_name
+  okta_base_url  = var.base_url
+  okta_api_token = var.api_token
 }
