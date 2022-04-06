7ab6b994266cabc0016985ad15832fd55b08f4a0
Valentine AKEM <valyakem@yahoo.co.uk>
2022-04-06 08:18:50 -0400

    Adding more files

--- okta-app.tf
+++ okta-app.tf
@@ -13,7 +13,7 @@ locals {
 provider "okta" {
   org_name  = var.org_name
   base_url  = var.base_url
-  api_token = "${local.api_token}"
+  api_token = local.api_token
 }
 
 resource "okta_group_schema_property" "example" {
