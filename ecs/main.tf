73cfc8b674b0164b9a06b62b827ca26f922d8807
Valentine AKEM <valyakem@yahoo.co.uk>
2022-04-16 22:02:21 -0400

    Updated changes on TF-Plan

--- ecs/main.tf
+++ ecs/main.tf
@@ -39,8 +39,6 @@ EOF
 }
 
 
-
-
 resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
